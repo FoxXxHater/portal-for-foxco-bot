6b63c5077e6d176adec3eedd5bcb55e9b5d277d7
FoxXxHater <info@foxco-network.de>
2023-06-09 11:08:33 +0200

    Fixed the redirect?
    forgot the ">" from the meta tag

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script>
 	// TODO: redirect to only guild, or display selector
 </script>
-<meta http-equiv="refresh" content="5; URL=https://dc-ticket.foxco-network.de/settings"
+<meta http-equiv="refresh" content="5; URL=https://dc-ticket.foxco-network.de/settings">
 <div>
 	<a href="/settings">Hier Klicken um auf die Seite zu gelangen</a>
 </div>
